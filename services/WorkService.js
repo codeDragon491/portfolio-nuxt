ef39719acfc66abf4dc6a637c5aeb5d85d4be2d8
codeDragon491 <trefasjulia@gmail.com>
2022-08-28 15:08:13 +0200

    Increase maxContentLength

--- services/WorkService.js
+++ services/WorkService.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 const apiClient = axios.create({
     baseURL: `https://my-json-server.typicode.com/codeDragon491/portfolio-data`, //on mock api (json-server) http://localhost:3004
     withCredentials: false,
+    maxContentLength: 100000000,
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json'
